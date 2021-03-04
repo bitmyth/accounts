0f65588dea92875b008dde70c3b096e694bf19e9
shangheng <shangheng@staff.weibo.com>
2021-03-04 21:36:00 +0800

    Change compose file

--- src/database/migrations/20210204142357_create_schema.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-create  schema accounts default  character set utf8mb4
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-drop schema accounts
--- +goose StatementEnd
